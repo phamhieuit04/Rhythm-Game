d69639182def5456c3d089beab29a922b09b169e
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-10-11 01:27:26 +0700

    Add piano models + outline shader graph + render outline logic

--- Assets/Game/Scripts/WorldMapScripts/Piano/Piano.cs
+++ Assets/Game/Scripts/WorldMapScripts/Piano/Piano.cs
@@ -1,19 +1,26 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Piano : MonoBehaviour, IInteract
 {
     [SerializeField] private Transform pianoVisual;
+    [SerializeField] private Transform pianoOutline;
 
     public bool CanInteract { get; set; }
 
+    private void Start()
+    {
+
+    }
+
     public void Interact()
     {
-        Debug.Log(gameObject.name);
+        SceneManager.LoadScene("GameScene");
     }
 
     public void ChangeVisual(bool isNear)
     {
-        Renderer renderer = pianoVisual.GetComponent<Renderer>();
-        renderer.material.color = isNear ? Color.white : Color.black;
+        pianoVisual.gameObject.SetActive(!isNear);
+        pianoOutline.gameObject.SetActive(isNear);
     }
 }
